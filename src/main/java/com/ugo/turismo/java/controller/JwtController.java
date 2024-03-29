6d9d70775c9b799c1d1cb095911a0d4fe7b2fe7b
Carlos Barrera Aleman <115600193+devpcarlos@users.noreply.github.com>
2024-03-28 21:58:29 -0500

    se crea un paquete controlador y se hace la clase JwtController con su logica

---
+++ src/main/java/com/ugo/turismo/java/controller/JwtController.java
@@ -0,0 +1,21 @@
+package com.ugo.turismo.java.controller;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JwtController {
+
+    @PostMapping("/generateToken")
+    public String generateToken(){
+        //Genera un token JWT con el sujeto "Carlos" y firma con el algoritmo HS256 y una clave "MiPrueba"
+        String token = Jwts.builder()
+                .setSubject("Carlos")
+                .signWith(SignatureAlgorithm.HS256, "MiPrueba")
+                .compact();
+        //Devuelve el token JWT generado como respuesta del servicio web
+        return token;
+    }
+}
