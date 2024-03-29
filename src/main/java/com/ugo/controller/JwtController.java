ed6ef9a2dcc390a6935ada524dfcdacdb5fef844
Jean Tapias <tranx6@gmail.com>
2024-03-29 14:05:03 -0700

    Testing KSearch configuration

--- src/main/java/com/ugo/controller/JwtController.java
+++ src/main/java/com/ugo/controller/JwtController.java
@@ -1,12 +1,20 @@
 package com.ugo.controller;
 
+import com.myzlab.k.KBuilder;
+import static com.ugo.k.generated.metadata.Tables.*;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class JwtController {
+    
+    private final KBuilder k;
 
     @PostMapping("/generateToken")
     public String generateToken(){
@@ -17,5 +25,14 @@ public class JwtController {
                 .compact();
         //Devuelve el token JWT generado como respuesta del servicio web
         return token;
+    }
+    
+    @GetMapping("/test")
+    public ResponseEntity test(){
+        return k.
+            select(EXPERIENCE.PRICE).
+            from(EXPERIENCE).
+            multiple().
+            buildResponse("experiences");
     }
 }
