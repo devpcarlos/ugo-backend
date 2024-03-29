a9ad57a1825503f40b14b4537dd32d2352b937c9
Jean Tapias <tranx6@gmail.com>
2024-03-29 13:56:30 -0700

    Tables class added

---
+++ src/main/java/com/ugo/k/generated/metadata/Tables.java
@@ -0,0 +1,24 @@
+package com.ugo.k.generated.metadata;
+
+public class Tables {
+
+	public static final ParamMetadata PARAM;
+	public static final RoleMetadata ROLE;
+	public static final AppUserMetadata APP_USER;
+	public static final BookingMetadata BOOKING;
+	public static final BookingStatusMetadata BOOKING_STATUS;
+	public static final ExperienceMetadata EXPERIENCE;
+	public static final ActivityTypeMetadata ACTIVITY_TYPE;
+	public static final AssessmentMetadata ASSESSMENT;
+
+	static {
+		PARAM = ParamMetadata.getInstance();
+		ROLE = RoleMetadata.getInstance();
+		APP_USER = AppUserMetadata.getInstance();
+		BOOKING = BookingMetadata.getInstance();
+		BOOKING_STATUS = BookingStatusMetadata.getInstance();
+		EXPERIENCE = ExperienceMetadata.getInstance();
+		ACTIVITY_TYPE = ActivityTypeMetadata.getInstance();
+		ASSESSMENT = AssessmentMetadata.getInstance();
+	}
+}
