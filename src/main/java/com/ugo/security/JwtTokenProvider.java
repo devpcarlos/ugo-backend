7892b868efb87769feb9159414fe0f2dd9765aa7
Carlos Barrera Aleman <115600193+devpcarlos@users.noreply.github.com>
2024-04-07 13:53:09 -0500

    creo el paquete security con la clase JwtTokenProvider

---
+++ src/main/java/com/ugo/security/JwtTokenProvider.java
@@ -0,0 +1,49 @@
+package com.ugo.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private int jwtExpiration;
+
+    public String generateToken(String email){
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + jwtExpiration * 1000);
+
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS256, jwtSecret)
+                .compact();
+    }
+
+    public  String getEmailFromToken(String token){
+        Claims claims = Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String token){
+        try{
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+}
