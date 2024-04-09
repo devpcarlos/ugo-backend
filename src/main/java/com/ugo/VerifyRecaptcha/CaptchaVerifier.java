2afaaa54beac8b8341e5c89530bd5d0060bf5c38
Carlos Barrera Aleman <115600193+devpcarlos@users.noreply.github.com>
2024-04-08 21:25:00 -0500

    elimina la declaración de secretKey y modifica el constructor para inyectar la clave desde las propiedades de la aplicación

--- src/main/java/com/ugo/VerifyRecaptcha/CaptchaVerifier.java
+++ src/main/java/com/ugo/VerifyRecaptcha/CaptchaVerifier.java
@@ -1,15 +1,20 @@
 package com.ugo.VerifyRecaptcha;
 
-import com.ugo.services.AuthService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-
+@Component
 public class CaptchaVerifier {
 
     //Clave secreta de reCAPTCHA proporcionada por google
-   private static final String secretKey = "6Lc8i6wpAAAAAFYjhqo8gFVhFqAsbx-bLraE8AG0";
+    private final String secretKey;
+
+    public  CaptchaVerifier(@Value("${google.recaptcha.secretKey}") String secretKey){
+        this.secretKey=secretKey;
+    }
 
     //Metodo para verificar el reCAPTCHA utilizando la API de Google reCAPTCHA
-    public static boolean verificarCaptcha(String captcha){
+    public final boolean verificarCaptcha(String captcha){
         // Construir la URL de la API de verificación de reCAPTCHA
         String url = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + captcha;
 
@@ -18,17 +23,17 @@ public class CaptchaVerifier {
         CaptchaResponse response = restTemplate.postForObject(url, null, CaptchaResponse.class);
 
         //Verificar si la respuesta de la API indica que el reCAPTCHA es valido
-        return response.isSucces();
+        return response.isSuccess();
     }
     //Clase para representar la respuesta de la API de verificacion de reCAPTCHA
     private static class CaptchaResponse{
         private boolean success;
 
-        public boolean isSucces(){
+        public boolean isSuccess(){
             return success;
         }
 
-        public void SetSuccess(boolean success){
+        public void setSuccess(boolean success){
             this.success = success;
         }
     }
